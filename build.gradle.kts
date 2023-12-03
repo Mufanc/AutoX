0380a97e438f8d72073c5004bf8ff7b6f508ac17
Mufanc <mufanc.xyz@gmail.com>
2023-12-03 20:59:33 +0800

    chore: add maven-publish plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,15 @@ plugins {
     alias(libs.plugins.org.jetbrains.kotlin.jvm) apply false
 }
 
+fun String.exec(): String {
+    return Runtime.getRuntime()
+        .exec(this.split(" ").toTypedArray())
+        .apply { waitFor() }
+        .inputStream.bufferedReader().readText().trim()
+}
+
+val libraryVersion by extra("git rev-list --count HEAD".exec())
+
 val androidMinSdkVersion by extra(26)
 val androidTargetSdkVersion by extra(34)
 val androidCompileSdkVersion by extra(34)
--- ksp/build.gradle.kts
+++ ksp/build.gradle.kts
@@ -1,14 +1,26 @@
 plugins {
     id("java-library")
+    id("maven-publish")
     alias(libs.plugins.org.jetbrains.kotlin.jvm)
 }
 
+val libraryVersion: String by rootProject.extra
 val androidSourceCompatibility: JavaVersion by rootProject.extra
 val androidTargetCompatibility: JavaVersion by rootProject.extra
 
 java {
     sourceCompatibility = androidSourceCompatibility
     targetCompatibility = androidTargetCompatibility
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            groupId = "xyz.mufanc.autox"
+            artifactId = "ksp"
+            version = libraryVersion
+        }
+    }
 }
 
 dependencies {
